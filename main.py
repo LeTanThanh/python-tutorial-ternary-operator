d09312c8557b686904dd35776ac32065e6d7d5ec
TanThanhLe <tanthanhcr95@gmail.com>
2024-04-26 23:33:15 +0700

    Introduction to Python Ternary Operator

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 if __name__ == "__main__":
-  print("Python Ternary Operator")
+  # Introduction to Python Ternary Operator
+
+  age = int(input("Enter your age: "))
+  # if age >= 18:
+  #   ticket_price = 20
+  # else:
+  #   ticket_price = 5
+
+  """
+  value_if_true if condition else value_if_false
+  """
+
+  ticket_price = 20 if age >= 18 else 5
+
+  print(f"The ticket price is {ticket_price}")
